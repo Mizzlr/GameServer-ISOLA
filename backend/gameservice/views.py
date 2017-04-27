b7f56536fe8dc9326f129c63616f0afb6fc965f6
Mushtaque Ahamed A <mushtaque@codenation.co.in>
2017-04-27 20:40:33 +0530

    Fixed minor typo in views.py

--- backend/gameservice/views.py
+++ backend/gameservice/views.py
@@ -13,14 +13,14 @@ class NestedContestHistoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.ContestHistory.objects.all()
     serializer_class = serializers.ContestHistorySerializer
 
-    def list(self, request, contest_id=None):
-        queryset = models.ContestHistory.objects.filter(contest_id=contest_id).order_by('creation_time')
+    def list(self, request, contest_pk=None):
+        queryset = models.ContestHistory.objects.filter(contest_pk=contest_pk).order_by('creation_time')
         serializer = serializers.ContestHistorySerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, id=None, contest_id=None):
-        queryset = models.ContestHistory.objects.filter(id=pk, contest_id=contest_id)
-        history = get_object_or_404(queryset, id=pk)
+    def retrieve(self, request, pk=None, contest_pk=None):
+        queryset = models.ContestHistory.objects.filter(pk=pk, contest_pk=contest_pk)
+        history = get_object_or_404(queryset, pk=pk)
         serializer = serializers.ContestHistorySerializer(history)
         return Response(serializer.data)
 
